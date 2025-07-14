1c9e045c30ffc0ff16c71c5aa41656971e1f7b8c
Rishikarpe <162286317+Rishikarpe@users.noreply.github.com>
2025-07-14 20:13:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'screens/home_screen.dart';
      WidgetsFlutterBinding.ensureInitialized();
      await Supabase.initialize(
        url: 'https://odpcqpdbjuowxxlznlaf.supabase.co', // Replace with your Supabase Project URL
-       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcGNxcGRianVvd3h4bHpubGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NzIwMDUsImV4cCI6MjA2ODA0ODAwNX0.21aJ5ZQPXz6I4-l6eY1vyXDXtng3v-RI0k9FSXAorAI', // Replace with your Supabase Anon Public Key
+       anonKey: '', // Replace with your Supabase Anon Public Key
      );
      runApp(MyApp());
    }
